18a47edfcc268948406b9ec624e58ff57e151d7d
RenIVII <motcongmotbangba1975@gmail.com>
2025-05-05 17:57:41 +0700

    change footer

--- views/main/footer.php
+++ views/main/footer.php
@@ -8,37 +8,36 @@
           <div class="col-lg-3 col-md-6 footer-contact">
             <h3>Trụ sở chính</h3>
             <p>
-              Z06 đường số 13 <br>
-              Phường Tân Thuận Đông, Quận 7<br>
-              TP. Hồ Chí Minh, Việt Nam <br><br>
-              <strong>Điện thoại:</strong> 090 918 34 46<br>
-              <strong>Email:</strong> start@vng.com.vn<br>
+              Tầng 6, Tòa nhà Capital Place,<br>
+              Số 29 Liễu Giai, Ngọc Khánh,<br>
+              Ba Đình, Hà Nội.<br><br>
+              <!-- <strong>Điện thoại:</strong> <br>
+              <strong>Email:</strong> .com.vn<br> -->
             </p>
           </div>
 
           <div class="col-lg-2 col-md-6 footer-links">
             <h4>Link liên kết</h4>
             <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="https://www.facebook.com/VNGCorporation.Page/">Facebook</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="https://www.youtube.com/channel/UCk2jT9v-BOmjbPZ08LUbTVA">Youtube</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="https://www.facebook.com/LifeAtVNG/">Life at VNG</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="https://www.facebook.com/groups/VNGRunClub/">VNG Run Club</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="https://www.facebook.com/GarenaMobileVN">Facebook</a></li>
+              <!-- <li><i class="bx bx-chevron-right"></i> <a href="https://www.facebook.com/LifeAtVNG/">Life at VNG</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="https://www.facebook.com/groups/VNGRunClub/">VNG Run Club</a></li> -->
             </ul>
           </div>
 
           <div class="col-lg-3 col-md-6 footer-links">
             <h4>Trung tâm hỗ trợ</h4>
             <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="https://hotro.zing.vn/">Hỗ trợ tài khoản Games</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="https://zalo.me/lienhe.html">Hỗ trợ tài khoản Zalo</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="https://hotro.garena.vn/">CSKH</a></li>
+              <!-- <li><i class="bx bx-chevron-right"></i> <a href="https://zalo.me/lienhe.html">Hỗ trợ tài khoản Zalo</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="https://www.vngcloud.vn/web/guest/trang-chu">Hỗ trợ dịch vụ Clouds</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="https://zalopay.vn/trung-tam-ho-tro">Hỗ trợ dịch vụ ZaloPay</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="https://zalopay.vn/trung-tam-ho-tro">Hỗ trợ dịch vụ ZaloPay</a></li> -->
             </ul>
           </div>
 
           <div class="col-lg-4 col-md-6 footer-newsletter">
             <h4>Đăng ký nhận tin tức mới nhất</h4>
-            <p>Những thông tin và tin tức mới nhất về VNGreen sẽ được gửi đến bạn qua email bạn điền dưới đây</p>
+            <p>Những thông tin và tin tức mới nhất về GARENA sẽ được gửi đến bạn qua email bạn điền dưới đây</p>
             <form action="" method="post">
               <input type="email" name="email"><input type="submit" value="Đăng ký">
             </form>
@@ -52,15 +51,15 @@
 
       <div class="me-md-auto text-center text-md-start">
         <div class="copyright">
-          &copy; Copyright <strong><span>VNGREEN</span></strong>. All Rights Reserved
+          &copy; Copyright <strong><span>GARENA</span></strong>. All Rights Reserved
         </div>
         <div class="credits">
-          Designed by <a href="index.php?page=admin&controller=layouts&action=index">VNGREEN</a>
+          Designed by <a href="index.php?page=admin&controller=layouts&action=index">GARENA</a>
         </div>
       </div>
       <div class="social-links text-center text-md-right pt-3 pt-md-0">
-        <a href="https://www.facebook.com/VNGCorporation.Page/" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="https://www.youtube.com/channel/UCk2jT9v-BOmjbPZ08LUbTVA" class="youtube"><i class="bx bxl-youtube"></i></a>
+        <a href="https://www.facebook.com/GarenaMobileVN" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <!-- <a href="https://www.youtube.com/channel/UCk2jT9v-BOmjbPZ08LUbTVA" class="youtube"><i class="bx bxl-youtube"></i></a> -->
       </div>
     </div>
   </footer><!-- End Footer -->
